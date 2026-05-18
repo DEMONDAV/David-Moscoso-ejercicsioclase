8abfb1f034c6fb757b205125d70ce2b9aaeb7872
DEMONDAV <david.moscoso@udla.edu.ec>
2026-05-18 12:21:19 -0500

    calculos para clacular promedio

--- ejer.c
+++ ejer.c
@@ -5,15 +5,22 @@ int main ()
     float v[5]={6,7,7.5,6.5,8};
     int tamanio=5;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < tamanio; i++)
     {
        printf("%.2f ", v[i]);
     }
-    calcularPromedio(v,tamanio);
+    CalcularPromedio(v,tamanio);
     
 }
 
 float CalcularPromedio (float *v,int tamanio)
 {
-
+  float suma,promedio; 
+  for (int i = 0; i < tamanio; i++)
+    {
+       suma=suma+v[i];
+    }
+  promedio = suma/tamanio;
+  
+  return promedio;
 }
