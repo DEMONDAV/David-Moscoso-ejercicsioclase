c0aec2c1741fe30b8ea9b4f893fec93e79fbc8d7
DEMONDAV <david.moscoso@udla.edu.ec>
2026-05-18 12:15:07 -0500

    prototipode fincion CalcularPromedio

--- ejer.c
+++ ejer.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
+float CalcularPromedio (float *v,int tamanio);
 int main ()
 {
     float v[5]={6,7,7.5,6.5,8};
+    int tamanio=5;
 
     for (int i = 0; i < 5; i++)
     {
        printf("%.2f ", v[i]);
     }
+    calcularPromedio(v,tamanio);
     
+}
+
+float CalcularPromedio (float *v,int tamanio)
+{
 
 }
